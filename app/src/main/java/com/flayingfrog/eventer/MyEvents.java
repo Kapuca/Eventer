492f82c97fcc0aa5679d3d381b520f40a9b68a7c
ANavarro96 <alejandronavarro96@hotmail.com>
2017-03-17 12:15:52 +0200

    Added the MyEvents page and swiping.Waiting for the database implementation and such. Kissess :****

--- app/src/main/java/com/flayingfrog/eventer/MyEvents.java
+++ app/src/main/java/com/flayingfrog/eventer/MyEvents.java
@@ -1,13 +1,22 @@
 package com.flayingfrog.eventer;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class MyEvents extends AppCompatActivity {
+import java.util.ArrayList;
+
+/*public class MyEvents extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,4 +40,118 @@ public class MyEvents extends AppCompatActivity {
         //create info
         //create button
     }
+}*/
+
+public class MyEvents extends AppCompatActivity {
+
+    private GestureDetector gestureDetector;
+    ArrayList<Event> Events;
+    ListView listView;
+    private static customView adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_my_events);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        listView=(ListView)findViewById(R.id.list);
+
+        Events= new ArrayList<>();
+
+        Events.add(new Event("The Wonder Years in Paris", "The wonder years concert", "The Wonder Years are going to a concert in France, don't miss it out!","September 23, 2008",getResources().getDrawable(R.drawable.twy)));
+        Events.add(new Event("Tiny Moving Parts in Kamnik", "Tiny moving parts concert", "Tiny Moving Parts are going to a concert in France, don't miss it out!","September 23, 2008",getResources().getDrawable(R.drawable.tmp)));
+        Events.add(new Event("Knuckle Puck in Madrid", "Knuckle Puck concert", "The Wonder Years are going to a concert in France, don't miss it out!","September 23, 2008",getResources().getDrawable(R.drawable.kp)));
+
+        adapter= new customView(Events,getApplicationContext());
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Event Event= Events.get(position);
+
+                Snackbar.make(view, Event.theInfo(),Snackbar.LENGTH_LONG)
+                        .setAction("No action", null).show();
+            }
+        });
+
+        gestureDetector = new GestureDetector(new MyEvents.SwipeGestureDetector());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my_events, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (gestureDetector.onTouchEvent(event)) {
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+
+    private void onLeftSwipe() {
+
+    }
+
+    private void onRightSwipe() {
+        Intent intent = new Intent(this, Test1.class);
+        startActivity(intent);
+
+    }
+
+    // Private class for gestures
+    private class SwipeGestureDetector extends GestureDetector.SimpleOnGestureListener {
+        // Swipe properties, you can change it to make the swipe
+        // longer or shorter and speed
+        private static final int SWIPE_MIN_DISTANCE = 120;
+        private static final int SWIPE_MAX_OFF_PATH = 200;
+        private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            try {
+                float diffAbs = Math.abs(e1.getY() - e2.getY());
+                float diff = e1.getX() - e2.getX();
+
+                if (diffAbs > SWIPE_MAX_OFF_PATH) {
+                    return false;
+                }
+
+                // Left swipe
+                if (diff > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    MyEvents.this.onLeftSwipe();
+
+                    // Right swipe
+                } else if (-diff > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    MyEvents.this.onRightSwipe();
+                }
+            } catch (Exception e) {
+                Log.e("YourActivity", "Error on gestures");
+            }
+            return false;
+        }
+    }
 }
+
